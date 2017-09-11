1e0644c1a6e4a63e77bccba3c419c4c868f7acf6
Jens Tinglev <tinglev@kth.se>
2017-09-11 13:29:53 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+__author__ = 'tinglev'
+
+from setuptools import setup
+
+setup(name='everest_util',
+      version='1.0',
+      description='Utility library for KTHs CD/CI pipeline',
+      url='http://github.com/KTH/everest_util',
+      author='tinglev',
+      author_email='tinglev@kth.se',
+      license='MIT',
+      packages=['everest_util'],
+      zip_safe=False)
