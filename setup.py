dd3f0ec43fc18f408dd2be9cd4dd32baa75d0019
Jens Tinglev <tinglev@kth.se>
2017-09-15 14:18:23 +0200

    Added manifest and fixed setup.py

--- setup.py
+++ setup.py
@@ -4,18 +4,16 @@ from setuptools import setup
 from pipenv.project import Project
 from pipenv.utils import convert_deps_to_pip
 
-pipfile = Project().parsed_pipfile
-requirements = convert_deps_to_pip(pipfile['packages'], r=False)
-#test_requirements = convert_deps_to_pip(pipfile['dev-packages'], r=False)
+lockfile = Project().lockfile_content
+requirements = [package for (package, _) in lockfile['default'].iteritems()]
 
 setup(name='everest_util',
-      version='1.0',
+      version='1.1',
       description='Utility library for KTHs CD/CI pipeline',
       url='http://github.com/KTH/everest_util',
-      author='tinglev',
+      author='Jens Tinglev',
       author_email='tinglev@kth.se',
       license='MIT',
       packages=['everest_util'],
       zip_safe=False,
-      install_requires=requirements,
-      setup_requires=['pipenv'])
+      install_requires=requirements)
