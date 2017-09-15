a9da074c04d99bb4f1b782bcd853084813963a55
Jens Tinglev <tinglev@kth.se>
2017-09-15 15:57:25 +0200

    Added submodules to packages in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ __author__ = 'tinglev@kth.se'
 
 from setuptools import setup
 from pipenv.project import Project
-from pipenv.utils import convert_deps_to_pip
 
 lockfile = Project().lockfile_content
 requirements = [package for (package, _) in lockfile['default'].iteritems()]
@@ -14,6 +13,6 @@ setup(name='everest_util',
       author='Jens Tinglev',
       author_email='tinglev@kth.se',
       license='MIT',
-      packages=['everest_util'],
+      packages=['everest_util', 'everest_util.systems', 'everest_util.entities'],
       zip_safe=False,
       install_requires=requirements)
