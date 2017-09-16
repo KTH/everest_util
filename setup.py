02059ee5adbe082a4d33ae5ab25632eb1df34853
Kenneth Reitz <me@kennethreitz.org>
2017-09-16 11:51:43 -0400

    Cleanup setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
 __author__ = 'tinglev@kth.se'
 
-from setuptools import setup
-from pipenv.project import Project
+from setuptools import setup, find_packages
 
-lockfile = Project().lockfile_content
-requirements = [package for (package, _) in lockfile['default'].iteritems()]
+requirements = [
+      'flask',
+      'requests',
+      'coloredlogs',
+      'pyyaml'
+]
 
-setup(name='everest_util',
+setup(
+      name='everest_util',
       version='1.1',
       description='Utility library for KTHs CD/CI pipeline',
       url='http://github.com/KTH/everest_util',
       author='Jens Tinglev',
       author_email='tinglev@kth.se',
       license='MIT',
-      packages=['everest_util', 'everest_util.systems', 'everest_util.entities'],
+      packages=find_packages(exclude=['test']),
       zip_safe=False,
-      install_requires=requirements)
+      install_requires=requirements
+)
